5318c830673fafca7601943e0d47d3ef176142c5
KhasrulAlam <94951568+KhasrulAlam@users.noreply.github.com>
2022-06-07 20:05:33 +0600

    Time: 177 ms (89.69%), Space: 43.4 MB (54.93%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.py
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.py
@@ -1,22 +1,15 @@
-class Solution:
-    # @param two ListNodes
-    # @return the intersected ListNode
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
     def getIntersectionNode(self, headA, headB):
-        if headA is None or headB is None:
-            return None
-
-        pa = headA # 2 pointers
-        pb = headB
-
-        while pa is not pb:
-            # if either pointer hits the end, switch head and continue the second traversal, 
-            # if not hit the end, just move on to next
-            pa = headB if pa is None else pa.next
-            pb = headA if pb is None else pb.next
-
-        return pa # only 2 ways to get out of the loop, they meet or the both hit the end=None
-
-# the idea is if you switch head, the possible difference between length would be countered. 
-# On the second traversal, they either hit or miss. 
-# if they meet, pa or pb would be the node we are looking for, 
-# if they didn't meet, they will hit the end at the same iteration, pa == pb == None, return either one of them is the same,None
+        curA, curB = headA, headB
+        while curA!= curB:
+            curA= headB if curA is None else curA.next
+            curB= headA if curB is None else curB.next
+        return curA
+            
+        
