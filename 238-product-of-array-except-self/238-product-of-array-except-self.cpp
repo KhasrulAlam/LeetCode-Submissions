d497b0a602c3ea512f82e27f25c1951698df1ac5
KhasrulAlam <94951568+KhasrulAlam@users.noreply.github.com>
2022-07-02 11:49:32 +0600

    Time: 38 ms (49.17%), Space: 23.9 MB (93.09%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -3,7 +3,11 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
        
         int n=nums.size();
+      
+        /*//O(N) space solution:
+      
         vector<int> pre(n),suff(n),res(n);
+        
         pre[0]=nums[0];
         suff[n-1]=nums[n-1];
         
@@ -17,7 +21,29 @@ public:
         res[n-1]=pre[n-2];
         
         for(int i=1;i<n-1;i++)
-            res[i]=suff[i+1]*pre[i-1];
+            res[i]=pre[i-1]*suff[i+1];
+        */
+        
+        
+        //O(1) space solution
+        
+        int suff=1,pre=1;
+        vector<int> res(n);
+        
+        res[0]=1;
+        
+        for(int i=1;i<n;i++)
+        {
+            pre*=nums[i-1]; 
+            res[i]=pre;
+        }
+        
+        for(int i=n-2;i>=0;i--)
+        {
+            suff*=nums[i+1];
+            res[i]*=suff;
+        }
+        
         
         return res;
     }
