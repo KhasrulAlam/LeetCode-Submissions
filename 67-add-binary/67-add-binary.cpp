4a496a9189977d59931ba4306afeadc1536c0dfc
KhasrulAlam <94951568+KhasrulAlam@users.noreply.github.com>
2022-09-16 12:45:26 +0600

    Time: 12 ms (8.80%), Space: 7 MB (12.81%) - LeetHub

--- 67-add-binary/67-add-binary.cpp
+++ 67-add-binary/67-add-binary.cpp
@@ -2,45 +2,20 @@ class Solution {
 public:
     string addBinary(string a, string b) {
         
-        int l_a = a.size()-1;
-        int l_b = b.size()-1;
-        
-        string ans="";
-        int c=0;
-            
-        while(l_a>=0 || l_b>=0){
-            
-            char aa, bb;
-            
-            if(l_a>=0 && l_b>=0)
-                aa=a[l_a], bb=b[l_b];
-            else if(l_a>=0)
-                aa=a[l_a], bb='0';
-            else 
-                aa='0',bb=b[l_b];
-            
-            if(aa=='0' && bb=='0')
-                ans+='0'+ c, c=0;
-            else if(aa=='0' && bb=='1' && c==0)
-                ans+='1', c=0;
-            else if(aa=='0' && bb=='1' && c==1)
-                ans+='0', c=1;
-            else if(aa=='1' && bb=='0' && c==0)
-                ans+='1', c=0;
-            else if(aa=='1' && bb=='0' && c==1)
-                ans+='0', c=1;
-            else if(aa=='1' && bb=='1' && c==0)
-                ans+='0', c=1;
-            else if(aa=='1' && bb=='1' && c==1)
-                ans+='1', c=1;
-            cout<<aa<<' '<<bb<<' '<<c<<" ="<<ans<<' '<<c<<endl;
-            l_a--, l_b--;
-        }
-        if (c==1)
-            ans+='1';
+        int c=0, ai= a.size()-1, bi= b.size()-1;
+        string add="";
+        while(ai>=0 || bi>=0 || c){
+            if(ai>=0)
+                c+=a[ai]-'0',ai--;
+            if(bi>=0)
+                c+=b[bi]-'0',bi--;
          
-        reverse(ans.begin(), ans.end());
+            add = char((c%2)+'0') + add ;
+            c/=2;
+            cout<<c<<endl;
+        }
         
-        return ans;
+        
+        return add;
     }
 };
